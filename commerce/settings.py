c2e650444a7bfde4ff3d9a327d15c786ff39c65a
fahridgn <fahridgn@gmail.com>
2023-08-05 18:33:37 +0300

    navbar çakışması giderildi

--- commerce/settings.py
+++ commerce/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+# eklentiler
+    'crispy_forms',
+    'crispy_bootstrap3',
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,4 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK='bootstrap3'
